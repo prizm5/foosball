96a1a93d688bf1370577938bf6f9e0ff0e278f9c
nc <collateralwebteam@us.ibm.com>
2015-05-11 13:55:00 -0400

    testing

--- pirtest2.py
+++ pirtest2.py
@@ -1,20 +1,48 @@
 # http://RasPi.tv/how-to-use-interrupts-with-python-on-the-raspberry-pi-and-rpi-gpio-part-3
 import RPi.GPIO as GPIO
 
+import time
+
+from neopixel import *
+
+# LED strip configuration:
+LED_COUNT      = 10      # Number of LED pixels.
+LED_PIN        = 18      # GPIO pin connected to the pixels (must support PWM!).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
+LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+
+
+def setScore(player,score):
+    if player%2==0:
+        start = 5
+    else:
+        start = 0
+
+    for i in 0..4:
+        strip.setPixelColor(i, Color(0, 0, 255))
+
+
 p2=0
 
 def play2():
     global p2
     if p2<10:
         p2=p2+1
+    setScore(2,p2)
     print(p2)
 
 p=0
+
+
+
 
 def play():
     global p
     if p < 10:
         p=p+1
+    setScore(1,p1)
     print(p)
 
 def reset():
@@ -39,6 +67,12 @@ def my_callback3(channel):
  
 
 def start():
+    
+    # Create NeoPixel object with appropriate configuration.
+    strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
+    # Intialize the library (must be called once before other functions).
+    strip.begin()
+
     GPIO.setmode(GPIO.BCM)
     
     # GPIO 23 & 17 set up as inputs, pulled up to avoid false detection.
