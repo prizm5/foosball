74af8f3b6a433b25aafd01ed1d036399b9697ff2
root <root@raspberrypi.(none)>
2015-05-12 08:52:56 -0400

    added exa,mple for score with lightstring

--- pirtest2.py
+++ pirtest2.py
@@ -1,4 +1,4 @@
-# http://RasPi.tv/how-to-use-interrupts-with-python-on-the-raspberry-pi-and-rpi-gpio-part-3
+# http://RasPi.itv/how-to-use-interrupts-with-python-on-the-raspberry-pi-and-rpi-gpio-part-3
 import RPi.GPIO as GPIO
 
 import time
@@ -13,6 +13,11 @@ LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
 LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
 
+# Create NeoPixel object with appropriate configuration.
+strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
+# Intialize the library (must be called once before other functions).
+strip.begin()
+
 
 def setScore(player,score):
     if player%2==0:
@@ -20,9 +25,9 @@ def setScore(player,score):
     else:
         start = 0
 
-    for i in 0..4:
-        strip.setPixelColor(i, Color(0, 0, 255))
-
+    for i in range(0,score):
+        strip.setPixelColor(start+i, Color(0, 0, 255))
+	strip.show()
 
 p2=0
 
@@ -34,9 +39,6 @@ def play2():
     print(p2)
 
 p=0
-
-
-
 
 def play():
     global p
@@ -68,25 +70,21 @@ def my_callback3(channel):
 
 def start():
     
-    # Create NeoPixel object with appropriate configuration.
-    strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
-    # Intialize the library (must be called once before other functions).
-    strip.begin()
-
+    strip.setPixelColor(0, Color(0, 0, 255))
     GPIO.setmode(GPIO.BCM)
     
     # GPIO 23 & 17 set up as inputs, pulled up to avoid false detection.
     # Both ports are wired to connect to GND on button press.
     # So we'll be setting up falling edge detection for both
-    GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    #GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
     
     # GPIO 24 set up as an input, pulled down, connected to 3V3 on button press
-    GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    #GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-    GPIO.add_event_detect(18, GPIO.FALLING, callback=my_callback, bouncetime=500)
-    GPIO.add_event_detect(4, GPIO.FALLING, callback=my_callback2, bouncetime=500)
-    GPIO.add_event_detect(24, GPIO.FALLING, callback=my_callback3, bouncetime=10)
+    #GPIO.add_event_detect(18, GPIO.FALLING, callback=my_callback, bouncetime=500)
+    GPIO.add_event_detect(17, GPIO.FALLING, callback=my_callback2, bouncetime=500)
+    #GPIO.add_event_detect(17, GPIO.FALLING, callback=my_callback3, bouncetime=10)
 
     try:
         global p
@@ -97,3 +95,5 @@ def start():
     GPIO.cleanup()           # clean up GPIO on normal exit
 
 start()
+
+
