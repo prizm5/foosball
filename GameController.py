aa05844f1ad01b68d10ad801ca120a86641e1eba
Nils Creque <ncreque@us.ibm.com>
2015-05-20 12:18:14 -0400

    adding player color flash

--- GameController.py
+++ GameController.py
@@ -19,9 +19,12 @@ class GameController(Configurable):
 
         sensor_config = self.ConfigSectionMap('sensor')
         sensors = SensorController(logger)
+
         sensors.AddSensor(sensor_config['sensor1port'], self.player1scored, sensor_config['sensor1bounce'])
         sensors.AddSensor(sensor_config['sensor2port'], self.player2scored, sensor_config['sensor2bounce'])
+
         sensors.AddButton(sensor_config['greenbuttonport'], self.start_instant_game, sensor_config['greenbuttonbounce'])
+        sensors.AddButton(sensor_config['redbuttonport'], self.start_instant_game, sensor_config['redbuttonbounce'])
 
         self.logger = logger
         self.game = Game()
@@ -30,16 +33,23 @@ class GameController(Configurable):
         self.run_idle()
         logger.info("%s Initialized", __name__)
 
+    def handle_red_button(self):
+        if self.state == GameState.instant_game or self.state == GameState.live_game:
+            self.led.clear()
+            self.run_idle()
+
     def start_instant_game(self, channel):
         self.game = Game()
         self.state = GameState.instant_game
         self.led.clear()
+        self.led.flash_player_colors()
         self.logger.info("New Instant Game Started")
 
     def start_live_game(self, game):
         self.state = GameState.live_game
         self.game = game
         self.led.clear()
+        self.led.flash_player_colors()
         self.logger.info("New Live Game Started between %s and %s", self.game.player1, self.game.player2)
 
     def player1scored(self, channel):
@@ -69,5 +79,6 @@ class GameController(Configurable):
             self.logger.info("Game has ended!")
 
     def run_idle(self):
+        self.state = GameState.idle
         self.led.rainbow_cycle()
 
--- ledcontroller.py
+++ ledcontroller.py
@@ -6,7 +6,7 @@ from neopixel import *
 class LedController(object):
     def __init__(self, logger):
         # LED strip configuration:
-        self.LED_COUNT      = 10      # Number of LED pixels.
+        self.LED_COUNT      = 20      # Number of LED pixels.
         self.LED_OFFSET     = self.LED_COUNT / 2
         self.LED_PIN        = 18      # GPIO pin connected to the pixels (must support PWM!).
         self.LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
@@ -14,7 +14,7 @@ class LedController(object):
         self.LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
         self.LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
         self.LEDS = self.make_led_values()
-
+        self.idle = True
         self.player1color = Color(0, 0, 255)
         self.player2color = Color(0, 255, 0)
         self.logger = logger
@@ -25,6 +25,23 @@ class LedController(object):
         # Initialize the library (must be called once before other functions).
         self.strip.begin()
         logger.info("%s Initialized", __name__)
+
+    def flash_player_colors(self):
+        for a in range(0, 3):
+            if a % 2 == 0:
+                p1 = 0
+                p2 = 1
+            else:
+                p1 = 1
+                p2 = 0
+
+            for i in (0, self.LED_OFFSET-1):
+                self.LEDS[i] = p1
+            for i in (self.LED_OFFSET, self.LED_COUNT-1):
+                self.LEDS[i] = p2
+            self._update_leds()
+            time.sleep(.25)
+        self.clear()
 
     def make_led_values(self):
         l={}
@@ -68,6 +85,7 @@ class LedController(object):
         self.strip.show()
 
     def clear(self):
+        self.idle = False
         for i in range(0, self.LED_COUNT):
             self.strip.setPixelColor(i, Color(0, 0, 0))
         self.strip.show()
@@ -84,8 +102,11 @@ class LedController(object):
             return Color(0, pos * 3, 255 - pos * 3)
 
     def rainbow_cycle(self, wait_ms=20, iterations=5):
+        self.idle = True
         """Draw rainbow that uniformly distributes itself across all pixels."""
         for j in range(256*iterations):
+            if self.idle == False:
+                break
             for i in range(self.strip.numPixels() - self.LED_OFFSET):
                 self.strip.setPixelColor(i, self.wheel(((i * 256 / self.LED_OFFSET) + j) & 255))
             for i in range(self.LED_OFFSET+1, self.strip.numPixels()):
