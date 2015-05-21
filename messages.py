135216d552f9a34301bc6e366218de8b49a65571
Nils Creque <ncreque@us.ibm.com>
2015-05-21 12:50:55 -0400

    updates

--- messages.py
+++ messages.py
@@ -3,18 +3,18 @@
 class Game(object):
     def __init__(self, id='0', player1='player1', player2='player2', player1score=0, player2score=0):
         self.Id = id
-        self.player1 = 'player1'
+        self.player1 = player1
         self.player1Score = player1score
         self.player1Color = '5DFC0A'
-        self.player2 = 'player2'
+        self.player2 = player2
         self.player2Score = player2score
         self.player2Color = '05E9FF'
 
 
 class GoalScored(object):
     def __init__(self, id, player):
-        self.Id = id
-        self.Player = player
+        self.gameid = id
+        self.player = player
 
 
 class GameQueued(object):
