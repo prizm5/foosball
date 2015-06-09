54ee7adbeebb565ff9441342a25ed20c48ef5d35
nc <collateralwebteam@us.ibm.com>
2015-06-09 14:30:54 -0400

    adding sounds controller

---
+++ sound-controller.py
@@ -0,0 +1,16 @@
+
+import os
+
+
+class SoundController(object):
+    def __init__(self, logger, sounds):
+        self.logger = logger
+
+    def play_scored(self):
+        os.system("mpg123 -q %s &", os.path.join(self.path, "EMDropFlourish.wav"))
+
+    def play_start(self):
+        os.system("mpg123 -q %s &", os.path.join(self.path, "CoinIn5.wav"))
+
+    def play_end(self):
+        os.system("mpg123 -q %s &", os.path.join(self.path, "piano.wav"))
