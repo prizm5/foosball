4f64557137a9b2142d3d53b8d9fe30174be811d4
Nils Creque <ncreque@us.ibm.com>
2015-06-11 12:49:21 -0400

    score updates

--- test.py
+++ test.py
@@ -3,4 +3,4 @@ from time import sleep
 
 p = os.path.join(os.path.dirname(os.path.realpath(__file__)), "wavs/CoinIn5.wav")
 print p
-os.system("aplay %s &".format(p))
+os.system("aplay {0} &".format(p))
