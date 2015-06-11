2300d7bf278d46ca2b481294f0b26bca8e37c76c
Nils Creque <ncreque@us.ibm.com>
2015-06-11 12:46:59 -0400

    score updates

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
+import os
+from time import sleep
 
-import sound-controller
 
-test = SoundController()
+os.system("aplay %s &".format(os.path.join(os.path.dirname(os.path.realpath(__file__)), "wavs/CoinIn5.wav")))
